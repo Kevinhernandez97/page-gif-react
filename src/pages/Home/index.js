c4393b087d4428403d82b436982d0aee529a4d59
Kevinhernandez97 <kmhs-18@hotmail.com>
2023-01-05 19:28:08 -0300

    create compoent Input and insert Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,24 +1,32 @@
+import { Input } from "../../components/Input";
 import { Link } from "wouter";
-import './Home.css'
 
+import "./Home.css";const POPULAR_GIFS = [
+  "Matrix",
+  "Cr7",
+  "HarryPotter",
+  "Colombia",
+  "Brasil",
+  "Annita",
+  "Badbunny",
+  "Coding",
+];
 
-const POPULAR_GIFS = ['Matrix','Cr7','HarryPotter','Colombia','Brasil','Annita','Badbunny','Coding']
-function Home () {
-
-    return (
-        <div className="listGif">
-            <h2 className="subTitle">Most popular</h2>
-            <ul>
-                {POPULAR_GIFS.map((popularGif) => (
-                <li key={popularGif}>
-                    <Link to={`/search/${popularGif}`}>
-                        {popularGif}
-                    </Link>
-                </li>
-                ))}
-            </ul>
-        </div>
-       )
+function Home() {
+  
+  return (
+    <div className="listGif">
+      <Input />
+      <h2 className="subTitle">Most popular</h2>
+      <ul>
+        {POPULAR_GIFS.map((popularGif) => (
+          <li key={popularGif}>
+            <Link to={`/search/${popularGif}`}>{popularGif}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export { Home }
+export { Home };
